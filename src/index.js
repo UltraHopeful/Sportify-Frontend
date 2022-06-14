8b00cce7e276de5ed23fbea8edde8bdc43914081
Aravind Jayanthi <ar687531@dal.ca>
2022-06-14 02:18:34 -0300

    Fixed Routing issues and merged Aravind Jayanthi code

--- src/index.js
+++ src/index.js
@@ -3,11 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
+
+import Home from "./pages/Home/index"
+import ReservationList from "./pages/Reservations";
+import userReservations from "./data/Data";
+import LogIn from "./pages/LogIn/index"
+import { ReservationDetails } from './pages/ReservationDetails';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// cite : https://reactrouter.com/docs/en/v6/components/routes
+// I used some of the code for routing
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* todo add path only which needs header */}
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="resources" element={<ReservationList reservations={userReservations} />} />
+          <Route path='resources/:reservationId' element={<ReservationDetails />} />
+          <Route path="store" element={<Home />} />
+          <Route path="membership" element={<Home />} />
+          <Route path="events" element={<Home />} />
+          <Route path="rewards" element={<Home />} />
+          <Route path="blogs" element={<Home />} />
+        </Route>
+        {/* todo add path only which doesn't need header */}
+        <Route path="/login" element={<LogIn />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
